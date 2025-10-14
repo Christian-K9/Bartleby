65d2a6686ff5c26d5b1b8bc09576ffd44f0d9acf
Christian-K9 <164112138+Christian-K9@users.noreply.github.com>
2025-10-14 19:57:18 -0400

    Update find_user.py

--- find_user.py
+++ find_user.py
@@ -22,6 +22,7 @@ print("Now Attempting To Login...")
 successes = []
 
 def find_csrf(html):
+    print("Creating Session...")
     m = re.search(r'<input[^>]+name=["\']?(csrf_token|csrf|token|_csrf)["\']?[^>]+value=["\']([^"\']+)["\']', html, re.I)
     return (m.group(1), m.group(2)) if m else (None, None)
     
